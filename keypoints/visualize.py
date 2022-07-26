851a6f215dcb207e107849d03ac5f070a9dd6f3b
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-26 11:30:28 +0900

    ff

--- keypoints/visualize.py
+++ keypoints/visualize.py
@@ -1,43 +1,49 @@
+import json
 from pathlib import Path
 
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.patches import Rectangle
 from PIL import Image
 
 from src.utils.cocoutils import bbox_from_binary_mask, calc_binary_mask_area, get_hand_mat_mask
 
-image_base = Path("data/coco_keypoints/train_images/")
-maks_base = Path("data/coco_keypoints/train_masks/")
+BASE = Path("data/coco_keypoints/")
+image_base = BASE / "train_images/"
+maks_base = BASE / "train_masks/"
 
 
-def view(jpeg_filename, png_filename):
+def view(anno, *, jpeg_filename):
     im = Image.open(image_base / jpeg_filename)
-    mask_img = Image.open(maks_base / png_filename)
-    hand, mat = get_hand_mat_mask(mask_img)
-    # plt.imshow(im)
-    # plt.show()
 
     fig, ax = plt.subplots()
 
     # create simple line plot
     ax.imshow(im)
-    ax.imshow(mask_img, alpha=0.6)
-
-    # add rectangle to plot
-    for target in (hand, mat):
-        x, y, w, h = bbox_from_binary_mask(target)
-        ax.add_patch(Rectangle((x, y), w, h, edgecolor="blue", fill=False, lw=2))
-        area = calc_binary_mask_area(target)
-        print(area)
-
+    # ax.imshow(mask_img, alpha=0.6)
+    lis = anno["keypoints"]
+    # print(anno["keypoints"])
+    for i in range(0, len(lis), 3):
+        x, y, visible = lis[i : i + 3]
+        if visible != 0:
+            plt.plot(x, y, marker="o", color="red")
     plt.show()
 
 
+def load_coco(path):
+    d = json.loads(path.read_text())
+    # print(d.keys())
+    return d["annotations"], d["images"]
+
+
 def main():
-    num = 2
-    jpeg_filename, png_filename = f"image_{num:06}.jpg", f"image_{num:06}.png"
-    view(jpeg_filename, png_filename)
+    annotations, images = load_coco(BASE / "train_annotations.json")
+    # for image in images:
+    #     print(image)
+    # for anno in annotations:
+    #     print(anno)
+
+    # jpeg_filename, png_filename = f"image_{num:06}.jpg", f"image_{num:06}.png"
+    view(annotations[0], jpeg_filename="image_000001.jpg")
 
 
 if __name__ == "__main__":
