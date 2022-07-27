838dc81316f82a855784b848e1d6cce65e76a21a
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-27 09:36:01 +0900

    fix

--- keypoints/train.py
+++ keypoints/train.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 from pathlib import Path
 
@@ -7,7 +8,7 @@ import flash
 from flash.image import KeypointDetectionData, KeypointDetector
 
 
-def main(data_dir):
+def main(*, data_dir, batch_size, max_epochs):
     # data_dir = "data/coco_keypoints"
     train_folder = data_dir / "train_images"
     train_ann_file = data_dir / "train_annotations.json"
@@ -15,8 +16,8 @@ def main(data_dir):
         train_folder=train_folder,
         train_ann_file=train_ann_file,
         val_split=0.1,
-        # transform_kwargs=dict(image_size=(128, 128)),
-        batch_size=2,
+        transform_kwargs=dict(image_size=(128, 128)),
+        batch_size=batch_size,
     )
     print(f"num of dataset: {len(datamodule.train_dataset)}")
     print(f"num_classes: {datamodule.num_classes}")
@@ -30,13 +31,18 @@ def main(data_dir):
     )
 
     # 3. Create the trainer and finetune the model
-    trainer = flash.Trainer(max_epochs=3)
+    trainer = flash.Trainer(max_epochs=max_epochs)
     trainer.finetune(model, datamodule=datamodule, strategy="freeze")
 
     # 5. Save the model!
     trainer.save_checkpoint("models/keypoint_detection_model.pt")
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_dir", type=Path, required=True)
+parser.add_argument("--batch_size", type=int, default=4, required=True)
+parser.add_argument("--max_epochs", type=int, default=10, required=True)
+
 if __name__ == "__main__":
-    p = Path(sys.argv[1])
-    main(p)
+    args = parser.parse_args()
+    main(data_dir=args.data_dir, batch_size=args.batch_size, max_epochs=args.max_epochs)
