9375084e7b8700096a86721693ccdd3db1fe5cd6
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-27 10:49:15 +0900

    fix

--- README.md
+++ README.md
@@ -7,7 +7,12 @@ $ source .venv/bin/activate.fish
 (.venv) $ pip install -r requirements/dev.txt
 ```
 
-## 推論
+## 学習
 ```
 python keypoints/train.py --data_dir data/coco_keypoints/ --batch_size 4 --max_epochs 10
+```
+
+## 推論
+```
+python keypoints/infer.py --model_file models/keypoint_ba16_ep250.pt --sample_image_dir data/samples/
 ```
--- keypoints/infer.py
+++ keypoints/infer.py
@@ -1,20 +1,20 @@
 # 4. Detect objects in a few images!
+import argparse
 import pickle
-import sys
 from pathlib import Path
 
 from flash import Trainer
 from flash.image import KeypointDetectionData, KeypointDetector
 
 
-def get_predict_files(base_dir):
-    iter = base_dir.glob("*.jpeg")
+def get_predict_files(sample_image_dir):
+    iter = sample_image_dir.glob("*.jpeg")
     iter = map(str, sorted(iter))
-    print(iter)
+    # print(iter)
     return list(iter)
 
 
-def main(model, predict_files):
+def infer(model, predict_files):
     datamodule = KeypointDetectionData.from_files(
         predict_files=predict_files,
         batch_size=4,
@@ -24,12 +24,19 @@ def main(model, predict_files):
     return predictions
 
 
-if __name__ == "__main__":
-    model_file = sys.argv[1]
-    print(f"model_file={model_file}")
+def main(*, model_file, sample_image_dir, output_file):
+    print(f"model_file={model_file} output_file={output_file}")
     model = KeypointDetector.load_from_checkpoint(model_file)
-    files = get_predict_files(Path(sys.argv[2]))
-    # print(files)
-    pred = main(model, files)
-    with open("predictions.pkl", "wb") as f:
-        pickle.dump(pred, f)
+    predict_files = get_predict_files(sample_image_dir)
+    pred = infer(model, predict_files)
+    output_file.write_bytes(pickle.dumps(pred))
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--model_file", type=Path, required=True)
+parser.add_argument("--sample_image_dir", type=Path, required=True)
+parser.add_argument("--output_pkl_file", type=Path, default="data/outputs/predictions.pkl")
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main(model_file=args.model_file, sample_image_dir=args.sample_image_dir, output_file=args.output_pkl_file)
