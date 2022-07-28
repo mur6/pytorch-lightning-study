71f66573e271cdae1f2245c84f16a263ea264c94
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-28 09:59:56 +0900

    ffff

---
+++ keypoints/pl/dataloader.py
@@ -0,0 +1,71 @@
+from pathlib import Path
+
+import pytorch_lightning as pl
+import torch
+from torch.utils.data import DataLoader, random_split
+
+from .dataset import MatDataset
+
+
+def train_collate_fn(batch):
+    data_list, label_list = [], []
+
+    for data, label in batch:
+        data_list.append(data)
+        label_list.append(label)
+
+    return data_list, label_list
+
+
+def test_collate_fn(batch):
+    data_list = []
+
+    for data in batch:
+        data_list.append(data)
+
+    return data_list
+
+
+class MatDataloader(pl.LightningDataModule):
+    def __init__(self, folder: str, batch_size: int, train_size: float):
+        super().__init__()
+        folder = Path(folder)
+        assert folder.is_dir(), f"{str(folder.resolve())} is not dir!"
+        self.batch_size = batch_size
+        self.train_size = train_size
+
+        self.train_dataset = ChessDataset(root_folder=folder, flag="train")
+        self.test_dataset = ChessDataset(root_folder=folder, flag="test")
+
+    def train_dataloader(self):
+        dataset_length = len(self.train_dataset)
+        num_training = int(dataset_length * self.train_size)
+
+        train_dataset, _ = random_split(
+            self.train_dataset,
+            lengths=[num_training, dataset_length - num_training],
+            generator=torch.Generator().manual_seed(0),
+        )
+
+        return DataLoader(
+            dataset=train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=4, collate_fn=train_collate_fn
+        )
+
+    def val_dataloader(self):
+        dataset_length = len(self.train_dataset)
+        num_training = int(dataset_length * self.train_size)
+
+        _, val_dataset = random_split(
+            self.train_dataset,
+            lengths=[num_training, dataset_length - num_training],
+            generator=torch.Generator().manual_seed(0),
+        )
+
+        return DataLoader(
+            dataset=val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=2, collate_fn=train_collate_fn
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset, batch_size=self.batch_size, shuffle=False, num_workers=2, collate_fn=test_collate_fn
+        )
---
+++ keypoints/pl/dataset.py
@@ -0,0 +1,77 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+
+# def xyxy2torchvision(coords: torch.tensor):
+#     new_coords = torch.empty(size=[coords.size()[0], 3])
+
+#     new_coords[:, 0] = coords[:, 0]
+#     new_coords[:, 1] = coords[:, 1]
+#     new_coords[:, 2] = 1
+
+#     return new_coords
+
+
+class ChessDataset(Dataset):
+    def __init__(self, root_folder: Path, flag: str = "train", transform=None):
+        assert flag in ["train", "test"]
+
+        super().__init__()
+        self.transform = transform
+        self.flag = flag
+
+        if self.flag == "train":
+            self.images = np.load(file=str(root_folder / "xtrain.npy"))
+            self.labels = np.load(file=str(root_folder / "ytrain.npy"))
+
+        else:
+            self.images = np.load(file=str(root_folder / "xtest.npy"))
+
+    def __getitem__(self, item):
+        x = self.images[item]
+
+        # https://discuss.pytorch.org/t/normalizing-images-between-0-and-1-with-totensor-doesnt-work/104363
+        if self.transform:
+            x = self.transform(x)
+        else:
+            transforms.ToTensor()(x) / 255.0
+
+        if self.flag == "train":
+            # Format of y is (num_instances, num_keypoints_in_instance, 3)
+            # 3 means that each keypoint should be represented in this form: [x, y, visibility],
+            # where visibility = 0 means that keypoint is not visible (visible - 1)
+            y = self.labels[item]
+            y = torch.as_tensor(y * 256.0).reshape(-1, 2)
+
+            boxes = torch.as_tensor([torch.min(y[:, 0]), torch.min(y[:, 1]), torch.max(y[:, 0]), torch.max(y[:, 1])])
+
+            points = xyxy2torchvision(y)
+            output = {
+                "boxes": boxes.unsqueeze(0),
+                "labels": torch.as_tensor(1).unsqueeze(0),
+                "keypoints": points.unsqueeze(0),
+            }
+            return x, output
+
+        else:
+            return x
+
+    def __len__(self):
+        return len(self.images)
+
+
+if __name__ == "__main__":
+    train_dataset = ChessDataset(root_folder=Path("../data"), flag="train")
+    image, labels = train_dataset[0]
+
+    image = image.permute(1, 2, 0).numpy()
+    labels = labels["keypoints"].squeeze()[:, :2]
+
+    plt.imshow(image)
+    plt.scatter(labels[:, 0], labels[:, 1], c="r")
+
+    plt.show()
