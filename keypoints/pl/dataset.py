c96e98aada36546e74ca9f46bfd3513151fa4dc1
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-28 11:08:47 +0900

    ff

--- keypoints/pl/dataset.py
+++ keypoints/pl/dataset.py
@@ -6,28 +6,25 @@ import torch
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset
 
-# def xyxy2torchvision(coords: torch.tensor):
-#     new_coords = torch.empty(size=[coords.size()[0], 3])
 
-#     new_coords[:, 0] = coords[:, 0]
-#     new_coords[:, 1] = coords[:, 1]
-#     new_coords[:, 2] = 1
+def xyxy2torchvision(coords: torch.tensor):
+    new_coords = torch.empty(size=[coords.size()[0], 3])
 
-#     return new_coords
+    new_coords[:, 0] = coords[:, 0]
+    new_coords[:, 1] = coords[:, 1]
+    new_coords[:, 2] = 1
+
+    return new_coords
 
 
-class ChessDataset(Dataset):
-    def __init__(self, root_folder: Path, flag: str = "train", transform=None):
-        assert flag in ["train", "test"]
-
+class MatDataset(Dataset):
+    def __init__(self, root_folder: Path, transform=None):
         super().__init__()
         self.transform = transform
-        self.flag = flag
 
         if self.flag == "train":
             self.images = np.load(file=str(root_folder / "xtrain.npy"))
             self.labels = np.load(file=str(root_folder / "ytrain.npy"))
-
         else:
             self.images = np.load(file=str(root_folder / "xtest.npy"))
 
@@ -40,32 +37,29 @@ class ChessDataset(Dataset):
         else:
             transforms.ToTensor()(x) / 255.0
 
-        if self.flag == "train":
-            # Format of y is (num_instances, num_keypoints_in_instance, 3)
-            # 3 means that each keypoint should be represented in this form: [x, y, visibility],
-            # where visibility = 0 means that keypoint is not visible (visible - 1)
-            y = self.labels[item]
-            y = torch.as_tensor(y * 256.0).reshape(-1, 2)
+        # Format of y is (num_instances, num_keypoints_in_instance, 3)
+        # 3 means that each keypoint should be represented in this form: [x, y, visibility],
+        # where visibility = 0 means that keypoint is not visible (visible - 1)
+        y = self.labels[item]
+        y = torch.as_tensor(y * 256.0).reshape(-1, 2)
 
-            boxes = torch.as_tensor([torch.min(y[:, 0]), torch.min(y[:, 1]), torch.max(y[:, 0]), torch.max(y[:, 1])])
+        boxes = torch.as_tensor([torch.min(y[:, 0]), torch.min(y[:, 1]), torch.max(y[:, 0]), torch.max(y[:, 1])])
 
-            points = xyxy2torchvision(y)
-            output = {
-                "boxes": boxes.unsqueeze(0),
-                "labels": torch.as_tensor(1).unsqueeze(0),
-                "keypoints": points.unsqueeze(0),
-            }
-            return x, output
-
-        else:
-            return x
+        points = xyxy2torchvision(y)
+        output = {
+            "boxes": boxes.unsqueeze(0),
+            "labels": torch.as_tensor(1).unsqueeze(0),
+            "keypoints": points.unsqueeze(0),
+        }
+        return x, output
 
     def __len__(self):
         return len(self.images)
 
 
 if __name__ == "__main__":
-    train_dataset = ChessDataset(root_folder=Path("../data"), flag="train")
+    root_dir = Path("data/")
+    train_dataset = MatDataset(root_dir=root_dir)
     image, labels = train_dataset[0]
 
     image = image.permute(1, 2, 0).numpy()
@@ -73,5 +67,4 @@ if __name__ == "__main__":
 
     plt.imshow(image)
     plt.scatter(labels[:, 0], labels[:, 1], c="r")
-
     plt.show()
