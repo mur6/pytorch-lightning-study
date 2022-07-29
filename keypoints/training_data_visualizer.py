ec50f64ba29cd9c405379c27f1a664cd9325b8d4
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-29 09:55:20 +0900

    fix

--- keypoints/training_data_visualizer.py
+++ keypoints/training_data_visualizer.py
@@ -11,11 +11,31 @@ from src.utils.cocoutils import bbox_from_binary_mask, calc_binary_mask_area, ge
 BASE = Path("data/coco_keypoints/")
 image_base = BASE / "train_images/"
 
+keypoint_names = [
+    "left-bottom",
+    "right-bottom",
+    "left-top",
+    "right-top",
+    "red[left-top]",
+    "blue[left-bottom]",
+    "brown[right-bottom",
+    "pink[right-top]",
+]
 
-def view(anno, *, jpeg_filename):
+color_names = [
+    "deeppink",
+    "cyan",
+    "coral",
+    "green",
+    "red",
+    "blue",
+    "brown",
+    "fuchsia",
+]
+
+
+def view(anno, *, ax, jpeg_filename):
     im = Image.open(image_base / jpeg_filename)
-
-    fig, ax = plt.subplots()
 
     # create simple line plot
     ax.imshow(im)
@@ -24,11 +44,11 @@ def view(anno, *, jpeg_filename):
     x, y, w, h = anno["bbox"]
     ax.add_patch(Rectangle((x, y), w, h, edgecolor="blue", fill=False, lw=2))
     # print(anno["keypoints"])
-    for i in range(0, len(kypt_list), 3):
+    for i, k_name, c_name in zip(range(0, len(kypt_list), 3), keypoint_names, color_names):
         x, y, visible = kypt_list[i : i + 3]
         if visible != 0:
-            ax.plot(x, y, marker="o", color="red")
-    plt.show()
+            ax.plot(x, y, marker="o", color=c_name)
+            ax.annotate(k_name, (x, y), color=c_name)
 
 
 def load_coco(path):
@@ -36,18 +56,21 @@ def load_coco(path):
     return d["annotations"], d["images"]
 
 
-def main(*, count):
+def main(*, row, col):
     annotations, images = load_coco(BASE / "train_annotations.json")
     # jpeg_filename, png_filename = f"image_{num:06}.jpg", f"image_{num:06}.png"
-    for i in range(count):
+    fig, axs = plt.subplots(row, col, figsize=(12, 7))
+    fig.tight_layout()
+    for i, ax in enumerate(axs.reshape(-1)):
         num = i + 1
-        view(annotations[i], jpeg_filename=f"image_{num:06}.jpg")
+        view(annotations[i], ax=ax, jpeg_filename=f"image_{num:06}.jpg")
+    plt.savefig("aa.png")
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--count", type=int, default=3)
-
+parser.add_argument("--fig_row", type=int, default=2)
+parser.add_argument("--fig_col", type=int, default=3)
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    main(count=args.count)
+    main(row=args.fig_row, col=args.fig_col)
