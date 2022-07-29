d47bd1ed1e699d60995198de7367ac3f90931cf8
MURAKI Taichi <muraki.taichi@gmail.com>
2022-07-29 10:55:09 +0900

    f

--- keypoints/inference_visualizer.py
+++ keypoints/inference_visualizer.py
@@ -6,23 +6,44 @@ from flash import DataKeys
 from matplotlib.patches import Rectangle
 from PIL import Image
 
+keypoint_names = [
+    "left-bottom",
+    "right-bottom",
+    "left-top",
+    "right-top",
+    "red[left-top]",
+    "blue[left-bottom]",
+    "brown[right-bottom",
+    "pink[right-top]",
+]
 
-def view(filepath, *, bbox, keypoint):
+color_names = [
+    "deeppink",
+    "cyan",
+    "coral",
+    "green",
+    "red",
+    "blue",
+    "brown",
+    "fuchsia",
+]
+
+
+def view(filepath, *, ax, bbox, keypoint):
     im = Image.open(filepath)
     im = im.resize((128, 128))
-    fig, ax = plt.subplots()
-
     # create simple line plot
     ax.imshow(im)
     # ax.imshow(mask_img, alpha=0.6)
     x, y, w, h = bbox["xmin"], bbox["ymin"], bbox["width"], bbox["height"]
     ax.add_patch(Rectangle((x, y), w, h, edgecolor="blue", fill=False, lw=2))
 
-    for item in keypoint:
+    for item, k_name, c_name in zip(keypoint, keypoint_names, color_names):
         x, y = item["x"], item["y"]
         # if visible != 0:
-        ax.plot(x, y, marker="o", color="red")
-    plt.show()
+        # ax.plot(x, y, marker="o", color="red")
+        ax.plot(x, y, marker="o", color=c_name)
+        ax.annotate(k_name, (x, y), color=c_name)
 
 
 if __name__ == "__main__":
@@ -31,8 +52,10 @@ if __name__ == "__main__":
         d_list = pickle.load(f)
         for d in d_list:
             item_list += d
-
-    for item in item_list:
+    row, col = 2, 3
+    fig, axs = plt.subplots(row, col, figsize=(12, 7))
+    fig.tight_layout()
+    for item, ax in zip(item_list[:6], axs.reshape(-1)):
         # pred_info = item[DataKeys.METADATA]
         # print(f"pred_info: {pred_info}")
         meta = item[DataKeys.METADATA]
@@ -44,5 +67,6 @@ if __name__ == "__main__":
         print(f"scores={scores}")
         input_meta = item[DataKeys.INPUT][DataKeys.METADATA]
         print(f"input={input_meta['filepath']}")
-        view(input_meta["filepath"], bbox=bbox, keypoint=keypoint)
+        view(input_meta["filepath"], ax=ax, bbox=bbox, keypoint=keypoint)
         print()
+    plt.savefig("b2.png")
